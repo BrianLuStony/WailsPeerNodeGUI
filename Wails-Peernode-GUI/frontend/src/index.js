1199d02fd27016ee9295094d670a1103d14242a8
Pritika Banerjee <pritika.banerjee@stonybrook.edu>
2024-02-17 21:35:53 -0500

    Added name to Sidebar

--- Wails-Peernode-GUI/frontend/src/index.js
+++ Wails-Peernode-GUI/frontend/src/index.js
@@ -1,47 +1,50 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
-import "./index.css";
-import "./sidebar.css";
+import "./styles/index.css";
+import "./styles/sidebar.css";
 
 const App = () => {
   return (
     <div className="app-container">
-      <div className="sidebar">
-        <div className="sidebar-section">
-          <div className="sidebar-label">Overview</div>
-          <button
-            className="sidebar-button"
-            onClick={() => {
-              /* TODO: make links to correct page  */
-            }}
-          >
-            Dashboard
-          </button>
-        </div>
-        <div className="sidebar-section">
-          <div className="sidebar-label">Data Access</div>
-          <button
-            className="sidebar-button"
-            onClick={() => {
-              /* trigger file upload */
-            }}
-          >
-            Upload File
-          </button>
-        </div>
-      </div>
+        <div className="sidebar">
+          <div className="sidebar-section">
+            <div className="sidebar-label-title">RatCoin</div>
 
-      <div className="main-content">
-        <header className="app-header">
-          <h1>Welcome to Peer App</h1>
-          <p>Your go-to app for seamless peer-to-peer interactions.</p>
-        </header>
-        <main></main>
-        <footer>
-          <p>Peer App © 2024</p>
-        </footer>
+          <div className="sidebar-section">
+            <div className="sidebar-labels">Overview</div>
+            <button
+                className="sidebar-button"
+                onClick={() => {
+                }}  //Todo: make links
+              >
+                Dashboard
+              </button>  
+          </div>
+          <div className="sidebar-section">
+            <div className="sidebar-labels">Data Access</div>
+            <button
+              className="sidebar-button"
+              onClick={() => {
+                /* trigger file upload */
+              }}
+            >
+              Upload File
+            </button>
+          </div>
+        </div>
+        </div>
+
+        <div className="main-content">
+          <header className="app-header">
+            <h1>Welcome to Peer App</h1>
+            <p>Your go-to app for seamless peer-to-peer interactions.</p>
+          </header>
+          <main></main>
+          <footer>
+            <p>Peer App © 2024</p>
+          </footer>
+        </div>
       </div>
-    </div>
   );
 };
 
