b853c4a1e7c7a466889afcfe026deea2d0bedc12
Adam Lipson <40801946+Adamlip1334@users.noreply.github.com>
2024-02-17 21:42:46 -0500

    added dashboard component

--- Wails-Peernode-GUI/frontend/src/index.js
+++ Wails-Peernode-GUI/frontend/src/index.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import { render } from "react-dom";
 import "./styles/index.css";
 import "./styles/sidebar.css";
+import Dashboard from "./components/dashboard";
 
 const App = () => {
   return (
     <div className="app-container">
-        <div className="sidebar">
+<div className="sidebar">
           <div className="sidebar-section">
             <div className="sidebar-label-title">RatCoin</div>
 
@@ -32,19 +33,10 @@ const App = () => {
             </button>
           </div>
         </div>
-        </div>
-
-        <div className="main-content">
-          <header className="app-header">
-            <h1>Welcome to Peer App</h1>
-            <p>Your go-to app for seamless peer-to-peer interactions.</p>
-          </header>
-          <main></main>
-          <footer>
-            <p>Peer App © 2024</p>
-          </footer>
-        </div>
       </div>
+
+      <Dashboard />
+    </div>
   );
 };
 
